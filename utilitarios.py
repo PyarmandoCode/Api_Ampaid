bee9639e5aa68d1f5b1c48c69956ff44b0f62734
Armando Ruiz <98427197+PyarmandoCode@users.noreply.github.com>
2023-12-04 17:52:24 -0500

    Update utilitarios.py

--- utilitarios.py
+++ utilitarios.py
@@ -1,14 +1,47 @@
-# Cargar los datos históricos de delitos
+import psycopg2
 import pandas as pd
 
-def generando_predicciones(lat,lon):
-    data = pd.read_csv('datos_delincuencia3.csv',sep=";")
-    pd_prediccion=data.query(f"data1=={lat} and data2 == {lon}")
-    columnas_deseadas=["latitud","longitud","fecha_robo","id_urbanizacion","urbanizacion","id_criticidad","desc_criticidad","estado_criticidad"]
-    pd_prediccion=pd_prediccion[columnas_deseadas]
+# Establecer la conexión a la base de datos PostgreSQL de Google Cloud
+def conexion_postgress():
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="Jhonnell2023",
+        host="34.136.193.201",
+        port=5432
+    )
+    return conn
 
-    lista=pd_prediccion.to_dict(orient='records')
-    return lista
-    
 
-#print(generando_predicciones(-12.05,-77.06))
+nombre_tabla = 'ampaid.t_hist_evento_delictivo'
+consulta_sql = f"SELECT * FROM {nombre_tabla};"
+
+
+def generando_predicciones(lat,lon,dia):
+   try:
+        #todo tomara la data que el modelo ha generado
+        data = pd.read_sql_query(consulta_sql,conexion_postgress())
+
+        # todo dandole un numero por dia de la semana
+        days = {0: 'Mon', 1: 'Tues', 2: 'Wed', 3: 'Thurs', 4: 'Fri', 5: 'Sat', 6: 'Sun'}
+
+        # todo creando el campo numero de la semana
+        data['fecha_evento'] = pd.to_datetime(data['fecha_evento'])
+        data['Numero_Del_Dia'] = data['fecha_evento'].dt.dayofweek
+        data['DiaDeLaSemana'] = data['Numero_Del_Dia'].apply(lambda x: days[x])
+
+        pd_prediccion = data[(data['latitud'].str.contains(lat)) & (data['longitud'].str.contains(lon))]
+        pd_prediccion_dia = pd_prediccion.loc[pd_prediccion['Numero_Del_Dia'] == dia]
+
+        #todo test_respectivos toda la prediccion
+        print(pd_prediccion)
+        # todo test_respectivos filtrado por fecha
+        print(pd_prediccion_dia)
+   except Exception as error:
+       print(f"Ocurrio un Error",error)
+
+   return pd_prediccion_dia
+
+print(generando_predicciones('-77.0166','-12.051379',0))
+#print(generando_predicciones('-12.044653','-77.054807'))
+#print(generando_predicciones('-12.05','-77.06'))
